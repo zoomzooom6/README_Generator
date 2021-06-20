62a580a421adc3121b2b77bfc463f318be074ce3
zoomzoom6 <tonyhuang107@live.ca>
2021-06-20 14:06:23 -0400

    take user input and generate README

--- Develop/index.js
+++ Develop/index.js
@@ -4,14 +4,67 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title? (Required)',
+        validate: projectInput => {
+            if (projectInput) {
+                return true;
+            } else {
+                console.log('Project title is required.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'about',
+        message: 'Provide information about your project: '
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide information about the usage information of your project: '
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Provide instructions on how to contribute to project: '
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Description on how to test your project: '
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select the license associated with your project',
+        choices: ['none', 'MIT', 'Apache', 'Mozilla', 'Unilicense', 'GNUv2.0']
+    }
+];
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {
     return new Promise((resolve, reject) => {
         fs.writeFile('./dist/' + fileName + ".md", data, err => {
             //if error, reject Promise and send error to Promise's catch method
-            if(err) {
+            if (err) {
                 reject(err);
                 return;
             }
@@ -26,6 +79,9 @@ const writeToFile = (fileName, data) => {
 //Gather user input 
 const promptUser = () => {
     let inputInfo = [];
+    // for (let i = 0; i < questions.length; i++) {
+    //     inquirer.prompt(questions[i]);
+    // }
     return inquirer.prompt([
         {
             type: 'input',
@@ -77,14 +133,32 @@ const promptUser = () => {
             type: 'list',
             name: 'license',
             message: 'Select the license associated with your project',
-            choices: ['none', 'MIT', 'Apache', 'Mozilla', 'Unilicense', 'GNUv2.0']
+            choices: ['none', 'MIT', 'Apache', 'Mozilla', 'Unilicense', 'GNUv3.0']
+        },
+        {
+            type: 'input',
+            name: 'features',
+            message: 'Enter the list of features in your project: '
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Provide any examples of tests of your project'
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Enter the names of anyone else that contributed to your project'
         }
+
     ]).then(infoInput => {
         inputInfo.push(infoInput);
         console.log(inputInfo);
-        return generateMarkdown(inputInfo);        
+        return generateMarkdown(inputInfo[0]);
+    }).then(readmeFile => {
+        return writeToFile(inputInfo[0].title, readmeFile);
     })
-}
+};
 
 // TODO: Create a function to initialize app
 const init = () => {
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -7,13 +7,35 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'MIT') {
+    let licenseURL = 'https://choosealicense.com/licenses/mit/';
+    return licenseURL;
+  } else if(license === 'Apace') {
+    let licenseURL = 'https://choosealicense.com/licenses/apache-2.0/';
+    return licenseURL;
+  } else if(license === 'Mozilla') {
+    let licenseURL = 'https://choosealicense.com/licenses/mpl-2.0/';
+    return licenseURL;
+  } else if(license === 'Unilicense') {
+    let licenseURL = 'https://choosealicense.com/licenses/unlicense/';
+    return licenseURL;
+  } else if(license === 'GNUv3.0') {
+    let licenseURL = 'https://choosealicense.com/licenses/gpl-3.0/';
+    return licenseURL;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  let licenseURL = renderLicenseLink(license);
+  let badge = renderLicenseBadge(license);
   if (license) {
-
+    return `${badge}
+    This project was made using ${license}, you can find more information about this license <a href="${licenseURL}" target="_blank">here</a>`
   } else {
     return '';
   }
@@ -21,8 +43,33 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  let license = renderLicenseSection(data.license);
   return `# ${data.title}
 
+  ## About the project
+  ${data.about}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Contribute](#how-to-contribute)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Credits
+  ${data.contributors}
+
+  ## License
+  ${license}
+
+  ## How to contribute
+  ${data.contribute}
 `;
 }
 
